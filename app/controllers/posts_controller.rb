81f6e5c56ac21ae264d391ed6fecdc62b4a9129e
shumy00 <syamu0855@gmail.com>
2021-05-08 23:53:41 +0900

    削除機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,13 @@ class PostsController < ApplicationController
   def create
     if @post = Post.create(memo: params[:memo])
       @result = @post.enzan(memo: params[:memo])
-      @result.save
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    if post.destroy
       redirect_to root_path
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to:"posts#new"
-  resources :posts, only: [:index, :new, :create] do
+  resources :posts, only: [:index, :new, :create, :destroy] do
   end
   
 end
